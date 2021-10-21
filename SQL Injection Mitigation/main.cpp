ec39a35893164db0fb9c09e6e42ca0875451ef12
Selene Clark <michellc@byui.edu>
2021-10-21 10:18:40 -0700

    Initial Commit

---
+++ SQL Injection Mitigation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SQL Injection Mitigation
+//
+//  Created by Selene on 10/21/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
